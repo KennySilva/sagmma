f4db0743dfc5f47abae42260fe340f56722aa2df
Kenny Pereira <keypasempre@gmail.com>
2017-02-01 17:35:42 -0100

    Atualizado

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sagmma\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Sagmma\Http\Requests;
+use Sagmma\Http\Controllers\Controller;
+use User;
+use Role;
+use Permission;
+
+class AdminController extends Controller
+{
+
+
+    public function index()
+    {
+        return "Estefanio Silva";
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
